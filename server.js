e22e62030ef63f01b779f8325c234de6cc65c8f7
RayanXweb <rohmanjakarta15@gmail.com>
2026-03-29 15:00:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+const express = require("express");
+const path = require("path");
 
+const app = express();
+
+// static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// route utama
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// dashboard route
+app.get("/dashboard", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/dashboard.html"));
+});
+
+// fallback (anti error 404 di vercel)
+app.get("*", (req, res) => {
+  res.redirect("/");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running..."));
